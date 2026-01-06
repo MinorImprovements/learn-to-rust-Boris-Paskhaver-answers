f04baf265eacfc472274f557049a4fb50acf64b5
welkerxa <welkerxa@amazon.com>
2026-01-06 15:23:56 -0800

    Completed datetime exercise

--- datetimes/src/main.rs
+++ datetimes/src/main.rs
@@ -1,5 +1,4 @@
-use chrono::prelude::*;
-use std::ops::{Add, Sub};
+use chrono::{prelude::NaiveDateTime, ParseError};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct DateTime {
@@ -8,12 +7,27 @@ struct DateTime {
 }
 
 impl DateTime {
-    fn new(datetime: &str, description: &str) -> Self {
-        let dt = NaiveDateTime::parse_and_remainder(datetime, "%Y-%m-%d %H:%M:%S");
-        Self {
+    fn new(datetime: &str, description: &str) -> Result<Self, ParseError> {
+        let dt = NaiveDateTime::parse_from_str(datetime, "%Y-%m-%d %H:%M:%S")?;
+        Ok(Self {
             datetime_stamp: dt,
             event_description: description.to_string(),
-        }
+        })
+    }
+
+    fn time_gap(&self, other: &Self) {
+        let dif = self
+            .datetime_stamp
+            .signed_duration_since(other.datetime_stamp);
+        let sec = dif.num_seconds() % 60;
+        let min = dif.num_minutes() % 60;
+        let hrs = dif.num_hours();
+        println!("Time since last event: {}h, {}m, {}s", hrs, min, sec);
+    }
+
+    fn display(&self) {
+        println!("Event Time: {:?}", self.datetime_stamp.to_string());
+        println!("Event Description: {:?}", self.event_description);
     }
 }
 
@@ -25,4 +39,27 @@ fn main() {
         ("2025-04-23 02:10:45", "Went to bed"),
         ("2025-04-25 13:00:03", "Resumed Rust study"),
     ];
+
+    let total_events = events
+        .into_iter()
+        .filter_map(|pair| {
+            let (datetime, description) = pair;
+            let datetime_and_event = DateTime::new(datetime, description);
+            match datetime_and_event {
+                Ok(result) => Some(result),
+                Err(_) => None,
+            }
+        })
+        .collect::<Vec<DateTime>>();
+
+    for event in 0..total_events.len() {
+        if event > 0 {
+            total_events[event].display();
+            total_events[event].time_gap(&total_events[event - 1]);
+            println!();
+        } else {
+            total_events[event].display();
+            println!();
+        }
+    }
 }
